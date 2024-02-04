7559a8b6108d782b2cca0913450d6456eebfd22c
Student A10 EE364 <ee364a10@eceprog5.ecn.purdue.edu>
2024-02-04 17:46:58 -0500

    resubmit part 5

--- Prelab04/getStudentsLargestCircuit.sh
+++ Prelab04/getStudentsLargestCircuit.sh
@@ -9,5 +9,5 @@
 # Write your sequence of statements here .
 
 echo "$(bash getCircuitsByStudent.sh $1 | xargs -I {} wc -c circuits/circuit_{}.dat | sort -hr | head -n 1 | egrep -o "[0-9]{2}-[0-9]-[0-9]{2}")"
-
+#          ^ get the stuents circuits       ^ pass those values into wc as files    ^ top-down  ^ grab 1st line       ^ grab the circuit name back out of the file name
 exit 0
