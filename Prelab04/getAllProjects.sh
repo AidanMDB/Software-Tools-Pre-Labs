3b595cecadb66c4ffdaee270f6ad58eb6ae1874c
Student A10 EE364 <ee364a10@eceprog5.ecn.purdue.edu>
2024-02-04 16:47:08 -0500

    finished part 3

---
+++ Prelab04/getAllProjects.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ######################################################
+# Author : < Aidan Dannhausen-Brun >
+# email : < adannhau@purdue.edu >
+# ID : < ee364a10 >
+# Date : < 2/4/2024 >
+# ######################################################
+# Write your sequence of statements here .
+
+echo "$(egrep -o "[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+" $1 | sort -u | tr -d '-')"
+
+exit 0
