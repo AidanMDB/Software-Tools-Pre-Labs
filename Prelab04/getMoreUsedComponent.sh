c00b47b45a3c846a8358b07ebaa4f24cbdeab00a
Student A10 EE364 <ee364a10@eceprog5.ecn.purdue.edu>
2024-02-04 17:57:49 -0500

    finished bonus problem

---
+++ Prelab04/getMoreUsedComponent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ######################################################
+# Author : < Aidan Dannhausen-Brun >
+# email : < adannhau@purdue.edu >
+# ID : < ee364a10 >
+# Date : < 2/4/2024 >
+# ######################################################
+# Write your sequence of statements here .
+
+frist_count=$(bash getComponentUseCount.sh $1)
+second_count=$(bash getComponentUseCount.sh $2)
+
+if [ "$frist_count" -lt "$second_count" ];
+then
+    echo "$2"
+else
+    echo "$1"
+fi
+
+exit 0
