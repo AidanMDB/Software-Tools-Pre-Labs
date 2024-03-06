fb8c51a2c0a0b626145226ee150c658a6a43d2ff
Student A10 EE364 <ee364a10@eceprog3.ecn.purdue.edu>
2024-03-06 12:06:49 -0500

    fixed flake 8 errors in test-prelab08.py

--- Prelab08/tests/test_prelab08.py
+++ Prelab08/tests/test_prelab08.py
@@ -5,24 +5,26 @@
 # Date : 3/3/2024
 # ######################################################
 
-import unittest
-import sys
-sys.path.insert(0, "./")
 from src import convertToBoolean as task1
 from src import convertToInteger as task2
 from src import getStreakProduct as task3
+import unittest
+import sys
+sys.path.insert(0, "./")
 
 # ######################################################
 # No Module - Level Variables or Statements !
 # ONLY FUNCTIONS BEYOND THIS POINT !
 # ######################################################
 
+
 class TestSuite(unittest.TestCase):
 
     def test_convertToBoolean(self):
 
         with self.subTest(name="Normal Input Test"):
-            expected_list = [True, False, False, False, False, False, False, False]
+            expected_list = [True, False, False, False, False,
+                             False, False, False]
             actual_list = task1.converToBoolean(128, 5)
             self.assertListEqual(expected_list, actual_list)
 
@@ -30,24 +32,24 @@ class TestSuite(unittest.TestCase):
             with self.assertRaises(ValueError):
                 task1.converToBoolean(10.543, 7)
 
-                
     def test_getStreakProduct(self):
 
         with self.subTest(name="180"):
             expected_list = ["549", "5491"]
-            actual_list = task3.getStreakProduct("54912346543439485756", 5, 180)
-            self.assertListEqual(expected_list, actual_list)
+            actual = task3.getStreakProduct("54912346543439485756", 5, 180)
+            self.assertListEqual(expected_list, actual)
 
         with self.subTest(name="400"):
-            expected_list = ["5445", "54451", "51445", "14455", "144551", "4455", "44551", "45514", "514514", "145145", "45145", ]
-            actual_list = task3.getStreakProduct("544514455145145798779789", 6, 400)
-            self.assertListEqual(expected_list, actual_list)
+            expected_list = ["5445", "54451", "51445", "14455", "144551",
+                             "4455", "44551", "45514", "514514", "145145",
+                             "45145", ]
+            actual = task3.getStreakProduct("544514455145145798779789", 6, 400)
+            self.assertListEqual(expected_list, actual)
 
         with self.subTest(name="300"):
             expected_list = []
-            actual_list = task3.getStreakProduct("11111111111111111111111111111", 3, 300)
-            self.assertListEqual(expected_list, actual_list)
-
+            actual = task3.getStreakProduct("11111111111111111111111", 3, 300)
+            self.assertListEqual(expected_list, actual)
 
     def test_convertToInteger(self):
 
@@ -74,6 +76,7 @@ class TestSuite(unittest.TestCase):
             expected_value = None
             actual_value = task2.convertToInteger(empty)
             self.assertEqual(expected_value, actual_value)
+
 
 # This block is optional and can be used for testing .
 # We will NOT look into its content .
